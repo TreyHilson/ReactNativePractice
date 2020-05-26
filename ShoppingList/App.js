e5df8ccd96ef22255e7ff5e733947aa38a23c7f0
Timeonlytells@gmail.com <Timeonlytells@gmail.com>
2020-05-25 22:53:41 -0400

    Added components Customized shopping list, added ui styling as well as delete functionality.

--- ShoppingList/App.js
+++ ShoppingList/App.js
@@ -1,34 +1,48 @@
-import React from 'react'
-import {View, Text, Image, StyleSheet} from 'react-native';
+import React, {useState} from 'react';
+import {View, Image, StyleSheet, Text, FlatList} from 'react-native';
+import Header from './components/header';
+import ListItem from './components/ListItem'
+import {v4 as uuidv4} from 'uuid';
 // View supports layout with flexbox, style, & some touch handling.
 
 const App = () => {
+  const [items, setItems] = useState([
+    {id: uuidv4(), text: 'Change U'},
+    {id: uuidv4(), text: 'Its A Dark World'},
+    {id: uuidv4(), text: 'Buttta'}  ]);
+
+  const deleteItem = (id) => {
+    setItems(prevItems => {
+      return prevItems.filter(item => item.id != id);
+    });
+  };
+
 return (
     <View style={styles.container}>
-      <Text style={styles.text}> 28 </Text>
-      <Image
-        source={{
-          uri: 'https://media.giphy.com/media/fWR4y5UdCty4E/200w_d.gif',
-        }}
-        style={styles.img}
-      />
+    <Header />
+      <FlatList
+        data={items}
+        renderItem={({item}) => <ListItem item={item} deleteItem={deleteItem} />}
+    />
     </View>
   );
 };
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    paddingTop: 70,
+    fontSize: 20,
   },
   text: {
     color: 'darkslateblue',
-    fontSize: 30,
+    fontSize: 28,
     fontFamily: 'Arial',
   },
   img: {
     width: 280,
     height: 280,
+    padding: 10,
   },
 });
 
---
+++ ShoppingList/components/header.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import {View, Image, Text, StyleSheet} from 'react-native';
+// View supports layout with flexbox, style, & some touch handling.
+
+const Header = ({title}) => {
+  return (
+    <View style={styles.header}>
+      <Text style={styles.text}>28</Text>
+      <Image
+        source={{
+          uri: 'https://media.giphy.com/media/Mp3uwtx0qLOGA/giphy.gif',
+        }}
+        style={styles.img}
+      />
+    </View>
+  );
+};
+const styles = StyleSheet.create({
+  header: {
+    height: 60,
+    paddingTop: 10,
+    backgroundColor: '#89b8ce',
+    width: 420,
+    alignItems: 'center',
+  },
+  text: {
+    color: '#fff',
+    fontSize: 28,
+    textAlign: 'center',
+  },
+  img: {
+    width: 330,
+    height: 330,
+    padding: 10,
+    marginTop: 300,
+  },
+});
+
+export default Header;
